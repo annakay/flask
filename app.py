f40c7abe58caa5a144219d3cc8af8ef976430433
Yoshihiko.A <annakay0727@gmail.com>
2023-07-25 22:27:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ def detect_faces(image_path, model):
 
 # Commented out IPython magic to ensure Python compatibility.
 # 1. YOLOv5のリポジトリをクローン
-!git clone https://github.com/ultralytics/yolov5.git
+git clone https://github.com/ultralytics/yolov5.git
 
 # 2. 必要なパッケージをインストール
-!pip install -r yolov5/requirements.txt
+pip install -r yolov5/requirements.txt
 
 # 3. データセットの準備
 # 自分で作成したデータセットをアップロードし、適切なフォルダに配置します。
@@ -54,7 +54,7 @@ def detect_faces(image_path, model):
 
 # 4. YOLOv5の訓練
 # %cd /content/yolov5/
-!python train.py --img 640 --batch 16 --epochs 100 --data dataset.yaml --weights yolov5s.pt --name my_model
+python train.py --img 640 --batch 16 --epochs 100 --data dataset.yaml --weights yolov5s.pt --name my_model
 
 # 学習済みの重みのパスを指定
 weights_path = 'best.pt'
